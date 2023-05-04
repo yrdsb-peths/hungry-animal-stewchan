e457f324e2b46f36e8db8e8c5694234d70842eca
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-05-04 08:27:01 -0400

    Update Elephant.java

--- Elephant.java
+++ Elephant.java
@@ -3,6 +3,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor{
 
     public void act() {
-        move(3);
+        if(Greenfoot.isKeyDown("d")){
+            move(5);
+        }
+        if(Greenfoot.isKeyDown("a")){
+            move(-5);
+        }
     }
 }
