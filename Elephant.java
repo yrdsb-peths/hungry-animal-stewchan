b06067cb9852870cf42baa7fe3d13b9b12d84088
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-05-17 08:35:19 -0400

    Animated elephant

--- Elephant.java
+++ Elephant.java
@@ -4,13 +4,27 @@ public class Elephant extends Actor{
     GreenfootSound sound = new GreenfootSound("elephantcub.mp3");
 
     GreenfootImage[] images = new GreenfootImage[8];
+    SimpleTimer animTimer = new SimpleTimer();
     
-    public Elephant(){        
+    public Elephant(){  
+        // Load elephant images
         for(int i = 0; i < images.length; i++) {
             images[i] = new GreenfootImage("elephant_idle/idle" + i + ".png");
             images[i].scale(100,100);
         }
         setImage(images[0]);
+        
+        // Reset the timer
+        animTimer.mark();
+    }
+    
+    private int imageIndex = 0;
+    public void animateElephant(){
+        if(animTimer.millisElapsed() > 100) {
+            imageIndex = (imageIndex + 1) % images.length;
+            setImage(images[imageIndex]);
+            animTimer.mark();
+        }
     }
     
     public void act() {
@@ -20,6 +34,9 @@ public class Elephant extends Actor{
         if(Greenfoot.isKeyDown("a")){
             move(-5);
         }
+        
+        // Animate the elephant
+        animateElephant();
         
         // Elephant eats the apple
         if (isTouching(Apple.class)){
